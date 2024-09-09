50e7a7e46709d497641c4fb4ac7199e64aeae5fc
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2024-09-09 20:09:32 +0330

    topaz: Welcome YAAP [2/2]
    Signed-off-by: itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_topaz.mk
+    $(LOCAL_DIR)/yaap_topaz.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_topaz-user \
-    lineage_topaz-userdebug \
-    lineage_topaz-eng
+    yaap_topaz-user \
+    yaap_topaz-userdebug \
+    yaap_topaz-eng
